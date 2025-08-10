dcf29f906a6deece599cc3f9bec3f4fb8253fd73
Markeljan Sokoli <Markeljan19@gmail.com>
2025-08-10 02:07:31 -0400

    verify account

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,13 @@
 import { Redis } from "@upstash/redis";
 
-if (!process.env.REDIS_URL || !process.env.REDIS_TOKEN) {
+const { REDIS_URL, REDIS_TOKEN } = process.env;
+if (!REDIS_URL || !REDIS_TOKEN) {
+  throw new Error(
+    "REDIS_URL or REDIS_TOKEN environment variable is not defined",
+  );
+}
+
+if (!REDIS_URL || !REDIS_TOKEN) {
   console.warn(
     "REDIS_URL or REDIS_TOKEN environment variable is not defined, please add to enable background notifications and webhooks.",
   );
